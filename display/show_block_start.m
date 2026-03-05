6dbb0eb0001ce9720d9be1268ff48ed7cf483bb1
Yeonglong Ay <albert99302@gmail.com>
2026-03-05 12:13:23 -0500

    Update show_block_start.m

--- display/show_block_start.m
+++ display/show_block_start.m
@@ -1,13 +1,24 @@
 function show_block_start(scr, cfg, blockNum, nBlocks, blockType)
-%SHOW_BLOCK_START  Display block number and type; wait for SPACE.
+%SHOW_BLOCK_START  Display block number prompt; wait for SPACE.
 %
 %   show_block_start(scr, cfg, blockNum, nBlocks, blockType)
+%
+%   Fixed:
+%     - Added isopen check so we get a clear error if window was closed
+%     - Defensive Screen('FillRect') before DrawFormattedText
 
-blockText = sprintf('Block %d of %d\n\nBlock Type: %s\n\nPress SPACE when ready.', ...
-    blockNum, nBlocks, cfg.blockTypeNames{blockType});
+if ~Screen('WindowKind', scr.win)
+    error('show_block_start: PTB window is not open (win=%d). Was sca called early?', ...
+          scr.win);
+end
 
 Screen('FillRect', scr.win, scr.black);
-DrawFormattedText(scr.win, blockText, 'center', 'center', scr.white);
+
+blockText = sprintf( ...
+    'Block  %d  of  %d\n\nBlock Type:  %s\n\nPress SPACE when ready.', ...
+    blockNum, nBlocks, cfg.blockTypeNames{blockType});
+
+DrawFormattedText(scr.win, blockText, 'center', 'center', scr.white, 60);
 Screen('Flip', scr.win);
 wait_for_key(cfg.keys.space);
 end

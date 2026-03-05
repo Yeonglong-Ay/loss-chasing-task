b4c25f4fc61999de1da353b4dc656304b6bc50e8
Yeonglong Ay <albert99302@gmail.com>
2026-03-05 11:31:24 -0500

    Create show_end_screen.m

---
+++ display/show_end_screen.m
@@ -0,0 +1,16 @@
+function show_end_screen(scr, cfg, totalReward)
+%SHOW_END_SCREEN  Display session complete screen with total earnings.
+%
+%   show_end_screen(scr, cfg, totalReward)
+
+endText = sprintf( ...
+    ['Session Complete!\n\n' ...
+     'Total Earned: $%d\n\n' ...
+     'Thank you for participating.\n\n' ...
+     'Press SPACE to exit.'], totalReward);
+
+Screen('FillRect', scr.win, scr.black);
+DrawFormattedText(scr.win, endText, 'center', 'center', scr.white);
+Screen('Flip', scr.win);
+wait_for_key(cfg.keys.space);
+end
